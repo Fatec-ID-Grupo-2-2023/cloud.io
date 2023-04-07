fa831c646730051732c225a8386125fe86acd5d3
Gianluca Dias De Micheli <de.micheli.gianluca@gmail.com>
2023-04-07 18:27:07 -0300

    create sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,18 @@ import { Box, IconButton } from "@mui/material";
 import AccountIcon from '../../assets/general/Account.svg';
 import MenuIcon from '../../assets/general/Menu.svg';
 import "./style.scss";
+import Sidebar from "../../pages/Sidebar";
+import { useState } from "react";
+import toggleDrawer from "../../helpers/toggleSidebar";
 
 export default function Header() {
+
+    const [sidebar, setSidebar] = useState(false);
+
     return (
         <Box className="header">
-            <IconButton color="primary" component="label">
+            <Sidebar open={sidebar} onClose={toggleDrawer(() => setSidebar(false))} />
+            <IconButton color="primary" component="label" onClick={toggleDrawer(() => setSidebar(true))}>
                 <img src={MenuIcon} alt="" />
             </IconButton>
             <IconButton color="primary" component="label">
