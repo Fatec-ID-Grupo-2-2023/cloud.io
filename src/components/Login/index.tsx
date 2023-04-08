575ab26a4412a3a0b82371d763fc079af4afc96c
Caique <caiquepatelliescapeline@gmail.com>
2023-04-08 17:23:43 -0300

    ajust login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { GoogleLogin } from 'react-google-login'
+import { useContext } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { useHistory } from 'react-router-dom';
+import { GlobalContext } from '../../contexts/GlobalContext';
 
 const clientId = "840694087672-q5jr4irk22t3ompetcsu4n9m0ods8ack.apps.googleusercontent.com"
 
 export default function Login() {
+    const history = useHistory();
+    const { setUser } = useContext(GlobalContext);
+
+    function onSuccess(response: any) {
+        // console.log(response)
+        history.push('/')
+        setUser(response)
+    }
 
     return (
         <div id="signInButton">
             <GoogleLogin
                 clientId={clientId}
                 buttonText='Login'
-                onSuccess={(res) => console.log(res)}
+                onSuccess={onSuccess}
                 onFailure={() => console.log('kk')}
                 cookiePolicy={'single_host_origin'}
                 isSignedIn={true}
