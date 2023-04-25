c1b5e8f2409efadd5789a9390d377b384701ca8c
Gianluca Dias De Micheli <de.micheli.gianluca@gmail.com>
2023-04-24 21:02:13 -0300

    create profile box

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,18 +6,21 @@ import { GlobalContext } from "../../contexts/GlobalContext";
 import toggleDrawer from "../../helpers/toggleSidebar";
 import Sidebar from "../Sidebar";
 import "./style.scss";
+import ProfileBox from "../ProfileBox";
 
 export default function Header() {
     const [sidebar, setSidebar] = useState(false);
+    const [profileBox, setProfileBox] = useState(false);
     const { user } = useContext(GlobalContext);
 
     return (
         <Box id="header">
+            <ProfileBox open={profileBox} onClose={() => setProfileBox(false)} />
             <Sidebar open={sidebar} onClose={toggleDrawer(() => setSidebar(false))} />
             <IconButton color="primary" component="label" onClick={toggleDrawer(() => setSidebar(true))}>
                 <img src={MenuIcon} alt="" />
             </IconButton>
-            <IconButton color="primary" component="label" onClick={() => console.log(user)}>
+            <IconButton color="primary" component="label" onClick={() => setProfileBox(true)}>
                 <Avatar
                     className="avatar"
                     src={user?.profileObj.imageUrl ?? AccountIcon}
